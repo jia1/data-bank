11601d13f88fd3fba27beae1bd9450f71993b7ec
Huiyan820 <huiyan.chen.2015@sis.smu.edu.sg>
2017-09-29 13:34:28 +0800

    Committing backend code first draft

--- back-end/FYP-backend/src/java/webServices/ApplicationConfig.java
+++
@@ -1,6 +0,0 @@
-package webServices;
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("API") // set the path to REST web services
-public class ApplicationConfig extends Application {}
